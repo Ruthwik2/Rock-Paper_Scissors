507b04b70d9ebce42a5f8d966dfc25a17ffbca5a
Ruthwik2 <ruthwikveldandi@gmail.com>
2026-06-22 11:10:57 +0530

    playRound function created it shows which player won in each round and calculates the score of player

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ function getHumanChoice(){
     return prompt("Enter your choice for Rock,Paper,Scissors");
 }
 
+function playRound(humanChoice,computerChoice){
+    let human_choice=humanChoice.toLowercase();
+    let computer_choice=computerChoice.toLowercase();
+    if(human_choice===computer_choice){
+        console.log("It's a tie");
+    }
+    else if(human_choice==="rock"&&computer_choice==="paper"){
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    }
+    else if(computer_choice==="rock"&&human_choice==="paper"){
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    }
+    else if(human_choice==="rock"&&computer_choice==="scissors"){
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    }
+    else if(human_choice==="scissors"&&computer_choice==="rock"){
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    }
+    else if(human_choice==="scissors"&&computer_choice==="paper"){
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+    }
+    else{
+        console.log("You lose! Paper beats Scissors");
+        computerScore++;
+    }
+
+}
+
