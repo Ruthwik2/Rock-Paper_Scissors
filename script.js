b9a53b4fba4c9bf9bf0ae28c70947a3b2b1399ee
Ruthwik2 <ruthwikveldandi@gmail.com>
2026-06-22 11:00:31 +0530

    Craeted gethuman choice function

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-console.log("Hello World");
+// console.log("Hello World");
+let humanScore=0,computerScore=0;
 function getComputerChoice(){
     let num=Math.floor(Math.random()*10);
     if(num%3==0) return "Rock";
     else if(num%2==0) return "Paper";
     else return "Scissor";
 }
+
+function getHumanChoice(){
+    return prompt("Enter your choice for Rock,Paper,Scissors");
+}
+
